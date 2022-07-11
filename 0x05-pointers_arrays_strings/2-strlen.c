cb18223cffc7b6611a3ea0448e41d2997c012800
AmfumuJoe <joekima98@gmail.com>
2022-07-11 21:27:05 +0200

    2. This report, by its very length, defends itself against the risk of being read

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -6,20 +6,12 @@
  *  Return: Always 0.
  */
 
-/**
-  * _strlen - Returns the length of a string
-  * @s: String to count
-  *
-  * Return: String length
-  */
 int _strlen(char *s)
 {
-	int c = 0;
+	int len;
 
-	for (; *s != '\0'; s++)
-	{
-		c++;
-	}
+	for (len = 0; *s != '\0'; len++)
+		s++;
 
-	return (c);
+	return (len);
 }
