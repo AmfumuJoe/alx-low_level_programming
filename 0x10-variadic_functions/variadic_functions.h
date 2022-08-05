8459ed63f8317c75f6a648bc366f28eec2d80c6a
AmfumuJoe <joekima98@gmail.com>
2022-08-05 10:45:15 +0200

    a function that returns the sum of all its parameters

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,13 +1,13 @@
-#ifindef VARIADIC_FUCTIONS_H
-#define VARIADIC_FUCTIONS_H
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
 
 #include <stdarg.h>
 
 /**
- * @struct printer - Anew struct type defining a printer.
+ * struct printer - A new struct type defining a printer.
  * @symbol: A symbol representing a data type.
- * @print: A fuction pointer to a fuction that prints
-            a data type corresponding to symbol.
+ * @print: A function pointer to a function that prints
+ *         a data type corresponding to symbol.
  */
 typedef struct printer
 {
@@ -17,8 +17,8 @@ typedef struct printer
 } printer_t;
 
 int sum_them_all(const unsigned int n, ...);
-void print_numbers(const char *separator, const unsigned int n, ..);
-void print_stings(const char *separator, const unsigned int n, ...);
-void print all(const char * const format, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
 #endif
