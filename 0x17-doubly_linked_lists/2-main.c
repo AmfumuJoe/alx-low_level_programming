f3dc8a0cf5a50a5c67cdf7fa0936c1f050d955f3
AmfumuJoe <joekima98@gmail.com>
2022-10-03 16:27:18 +0200

    printf solution

--- 0x17-doubly_linked_lists/2-main.c
+++ 0x17-doubly_linked_lists/2-main.c
@@ -1,15 +1,9 @@
 #include <stdlib.h>
-
 #include <string.h>
-
 #include <stdio.h>
-
 #include "lists.h"
 
-
-
 /**
-
  * main - check the code
  * Return: Always EXIT_SUCCESS.
  */
--- 0x17-doubly_linked_lists/3-main.c
+++ 0x17-doubly_linked_lists/3-main.c
@@ -1,18 +1,13 @@
 #include <stdlib.h>
-
 #include <string.h>
-
 #include <stdio.h>
-
 #include "lists.h"
 
 
 
 /**
-
  * main - check the code
  * Return: Always EXIT_SUCCESS.
-
  */
 
 int main(void)
